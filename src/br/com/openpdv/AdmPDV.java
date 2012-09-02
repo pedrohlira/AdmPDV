bd04ffeb908ef8e828369852aad19d8d8c94ba7b
Pedro Lira <pedroh.lira@gmail.com>
2012-09-02 11:24:32 -0300

    Versao 1.0

--- src/br/com/openpdv/AdmPDV.java
+++ src/br/com/openpdv/AdmPDV.java
@@ -1,3 +1,4 @@
+package br.com.openpdv;
 
 import java.io.Console;
 import java.io.File;
@@ -9,7 +10,7 @@ import org.jasypt.util.text.BasicTextEncryptor;
 
 /**
  * Classe que contem a chave privada e criptograva o arquivo auxiliar ou gera
- * uma senha usando a chave privada.
+ * uma senha usando a chave privada ou gera o cacerts de todos os estados.
  *
  * @author Pedro H. Lira
  */
@@ -21,11 +22,23 @@ public class AdmPDV {
     private static final String VALOR = "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAKoKq4/wwLUaboZ0FJWcHw70RrPpgXxYa87Zn6hx0ddZVkah/7GZLK11IyfTv2SPPu5yBoL2zSgLcZmQTbaRE8FIV/xp6FVOKl6vKVLJToTRL6sAK+y4tn3v77SMhO3jcAF+UEfkuYA0NhDuP6BSX8xeKsf0QtKXfOUPZKVRWDRJAgMBAAECgYBqmfHgdXB6mS19QTy+ZnfaKi5BEDzLNauwrCI5udFyDmHWOi1Qq9SkkfEOQOOWKW+skPOgGxWW60W2ng76S41z8zgE60hPpHbG377L97+gHx2W2pQqpKqT+HY3nKKKg5LXaOXcJpKg+qWxHuB5HIg1++v69yIphwy3H9ezEMom+QJBANWXzXvegBvAgPP1CSjQqOohljJu1DQvNkycDR08igt6u1hrKE+5TnqtH/oTct2HMr3Dytt8WUwZhIMzRfRkpQMCQQDLzU356B5EbjT43drzk3sVUIO8gqFnZd27dr7h2rhez09Fhvs9uhNCrxAau0MyHjuQqdcIK1jMo5p9q3292IHDAkEAtat+LQNctz4O8HDUL5D6etnCZHb7qr8VUifQx7M8PvNO8a+DA/w7EgC6l/t+0Yky0VD8855aXW2+1dEAiRQpfQJADeQhg34pyfpFDA17Jg0QX6SMRiYVQEn2Mhfm7NgOpw+0VIXdzBhJxcrO6zOGYf0jjDm3WWwy8kfYeOZefLksJwJBANO/nX1TLdcumFQnV0C4zfBWU6daCYYPO7zhYS83SCr66uyT7agJOAB91jEDPxUjrpynj5IpW2kh7vNy1zpA/zc=";
 
     /**
-     * Metodo de acao externa usado somente para criptografar o arquivo
-     * auxiliar.
+     * Construtor padrao.
+     */
+    private AdmPDV() {
+    }
+
+    /**
+     * Metodo de acao externa usado para criptografar o arquivo auxiliar, senha
+     * e gerar cacerts.
      *
-     * @param args um array sendo o primeiro o path completo do arquivo
-     * auxiliar.properties
+     * <p/>
+     * [opcao] = arquivo, para criptogravar o arquivo auxiliar.properties.
+     * <p/>
+     * [opcao] = senha, para criptogravar uma senha informada.
+     * <p/>
+     * [opcao] = cacerts, para gerar o arquivo NFeCacerts de todos os estados.
+     *
+     * @param args um array sendo o primeiro parametro uma das opcoes acima.
      */
     public static void main(String[] args) {
         if (args.length == 1) {
@@ -78,18 +91,23 @@ public class AdmPDV {
                 String senha = seguranca.encrypt(new String(pws));
                 System.out.println("Senha criptografada: " + senha);
                 System.exit(0);
+            } else if (args[0].equalsIgnoreCase("cacerts")) {
+                Cacerts.gerar();
+                System.exit(0);
             }
         }
 
         System.out.println("Falta a informar a [opcao] de utilizacao.");
         System.out.println("\t[opcao] = arquivo, para criptogravar o arquivo auxiliar.properties.");
         System.out.println("\t[opcao] = senha, para criptogravar uma senha informada.");
+        System.out.println("\t[opcao] = cacerts, para gerar o arquivo NFeCacerts de todos os estados.");
     }
 
     /**
      * Metodo que compara a senha informada com a senha de ADM salva.
      *
      * @param senha a senha digitada no console.
+     *
      * @return true se for igual , false se nao for igual.
      */
     private static boolean validarSenha(String senha) {
